4a64331a894a19f37490b4ee672fa767d7de2578
daniilv-me <me@daniilv.me>
2022-09-14 00:49:39 -0400

    Week2

---
+++ server.js
@@ -0,0 +1,26 @@
+const connect = require('connect');
+const app = connect();
+
+function logger(req, res, next){
+    console.log(req.method, req.url);
+
+    next();
+}
+
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+};
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('GoodBye World');
+};
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000/');
